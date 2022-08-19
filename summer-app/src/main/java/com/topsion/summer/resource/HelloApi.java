f226846f180327c112e2734461c4a94aafd4d4f6
pengkun.wen <pengkun.wen@essexlg.com>
2022-08-19 09:29:11 +0800

    Add sample example

--- summer-app/src/main/java/com/topsion/summer/resource/HelloApi.java
+++ summer-app/src/main/java/com/topsion/summer/resource/HelloApi.java
@@ -1,8 +1,14 @@
 package com.topsion.summer.resource;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
+@RestController
 public class HelloApi {
 
+    @GetMapping
+    public String sayHello() {
+        return "Hello World";
+    }
 }
