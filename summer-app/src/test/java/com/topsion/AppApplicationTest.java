9926403eb6abc91d6e2e6da795a06e09a9d3c958
pengkun.wen <pengkun.wen@essexlg.com>
2022-08-22 08:37:20 +0800

    Add application test

---
+++ summer-app/src/test/java/com/topsion/AppApplicationTest.java
@@ -0,0 +1,10 @@
+package com.topsion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+class AppApplicationTest {
+    @Test
+    public void contextLoads() {
+    }
+}
---
+++ summer-core/src/test/java/com/topsion/CoreApplicationTest.java
@@ -0,0 +1,11 @@
+package com.topsion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTest {
+    @Test
+    public void contextLoads() {
+    }
+}
