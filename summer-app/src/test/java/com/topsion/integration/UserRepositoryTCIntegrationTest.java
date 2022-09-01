4c2a034a5f0152840384b2febd1b8452780bf69e
pengkun.wen <wensir_top@163.com>
2022-09-01 09:18:46 +0800

    Add testcontainer

--- summer-app/src/test/java/com/topsion/integration/UserRepositoryTCIntegrationTest.java
+++ summer-app/src/test/java/com/topsion/integration/UserRepositoryTCIntegrationTest.java
@@ -1,6 +1,7 @@
-package com.topsion;
+package com.topsion.integration;
 
 
+import org.junit.ClassRule;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.util.TestPropertyValues;
@@ -8,19 +9,19 @@ import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @ContextConfiguration(initializers = {UserRepositoryTCIntegrationTest.Initializer.class})
-public class UserRepositoryTCIntegrationTest extends UserRepositoryCommonIntegrationTests{
+public class UserRepositoryTCIntegrationTest{
 
     @ClassRule
     public static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer("postgres:11.1")
             .withDatabaseName("integration-tests-db")
             .withUsername("sa")
             .withPassword("sa");
-    static class Initializer
-            implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+    static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
         public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
             TestPropertyValues.of(
                     "spring.datasource.url=" + postgreSQLContainer.getJdbcUrl(),
@@ -29,4 +30,5 @@ public class UserRepositoryTCIntegrationTest extends UserRepositoryCommonIntegra
             ).applyTo(configurableApplicationContext.getEnvironment());
         }
     }
+
 }
