edfe4395173ef63246592b67f1ebd004c4e5d12c
pengkun.wen <wensir_top@163.com>
2022-09-09 08:38:54 +0800

    add jpa

--- summer-app/src/main/java/com/topsion/summer/repository/entity/Product.java
+++ summer-app/src/main/java/com/topsion/summer/repository/entity/Product.java
@@ -1,4 +1,15 @@
 package com.topsion.summer.repository.entity;
 
+import lombok.Data;
+import org.springframework.context.annotation.Primary;
+
+import javax.persistence.Table;
+
+@Data
+@Table(name = "product_list")
 public class Product {
+    private Long id;
+    private String name;
+    private String category;
+    private String code;
 }
